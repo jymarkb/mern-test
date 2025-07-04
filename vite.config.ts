bccd609873ef5046c11ad160a9ee766a60f6df05
arcobaleno <jymark.borja@gmail.com>
2025-07-05 02:10:55 +0800

    update: assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import {resolve} from 'path';
+
+const root = resolve(__dirname, 'src')
 
 export default defineConfig({
   plugins: [react()],
-  root: 'src',
+  root,
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
   server: {
     port: 3000
   }
