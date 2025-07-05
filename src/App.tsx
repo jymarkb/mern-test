5b9db52ff59381cbd5d969e54bb51fc16d91b736
arcobaleno <jymark.borja@gmail.com>
2025-07-05 20:05:36 +0800

    update: app template with toast sooner

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './Route/react/Router';
+import { Toaster } from 'sonner';
 const App = () => {
     return (
       <BrowserRouter>
         <AppRoutes/>
+        <Toaster richColors />
       </BrowserRouter>
     );
   }
