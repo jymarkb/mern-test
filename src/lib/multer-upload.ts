82f3ff4d2c971eb2f93d727d111e39190017b2dc
arcobaleno <jymark.borja@gmail.com>
2025-07-05 19:59:23 +0800

    fix: lib utils

---
+++ src/lib/multer-upload.ts
@@ -0,0 +1,19 @@
+import multer from "multer";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const storage = multer.diskStorage({
+  destination: function (_req, _file, cb) {
+    cb(null, path.join(__dirname, "../public/upload"));
+  },
+  filename: function (_req, file, cb) {
+    const ext = path.extname(file.originalname);
+    const uniqueName = Date.now() + ext;
+    cb(null, uniqueName);
+  },
+});
+
+export const upload = multer({ storage });
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,71 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { Request } from "express";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export type MulterRequest = Request & {
+  file?: Express.Multer.File;
+};
+
+export type EmployeeType = {
+  id: number;
+  firstname: string;
+  lastname: string;
+  username: string;
+  email: string;
+  number: string;
+  photo: string;
+  countryId: number;
+  typeId: number;
+  country: {
+    id: number;
+    code: string;
+    name: string;
+  };
+  type: {
+    id: number;
+    type: string;
+  };
+};
+
+export type TableEmployee = {
+  photo: string;
+  name: string;
+  username: string;
+  country: string;
+  email: string;
+  id: number;
+  type:string
+};
+
+export type EmployeeORMType = {
+  id: number;
+  firstname: string;
+  lastname: string;
+  username: string;
+  email: string;
+  number: string;
+  countryId: number;
+  typeId: number;
+};
+
+export type CountryType = {
+  id: number;
+  code: string;
+  name: string;
+};
+
+export type AccountType = {
+  id: number;
+  type: string;
+};
+
+export type DeleteDialogType = {
+  isOpen: boolean;
+  onOpenChange: (open: boolean) => void;
+  handleClick: () => void;
+  data: TableEmployee;
+};
